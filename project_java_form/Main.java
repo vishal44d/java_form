782b7db88e6fc387e32066aeacbea9ce5c02c79a
vishal44d <98634369+vishal44d@users.noreply.github.com>
2022-06-16 23:25:00 +0530

    Add files via upload

---
+++ project_java_form/Main.java
@@ -0,0 +1,10 @@
+package project_java_form;
+
+
+public class Main {
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+        MyWindow w=new MyWindow();
+    }
+	
+}
